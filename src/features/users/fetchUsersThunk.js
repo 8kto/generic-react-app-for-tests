d70a15303387d12932c935214fa6be7b767e2f99
Okto <web@axisful.info>
2022-02-13 14:14:50 +0100

    Add docs

--- src/features/users/fetchUsersThunk.js
+++ src/features/users/fetchUsersThunk.js
@@ -1,5 +1,8 @@
 import { add } from './usersSlice'
 
+/**
+ * @link https://redux.js.org/usage/writing-logic-thunks
+ */
 export const fetchUsers = () => async (dispatch) => {
   const response = await fetch('https://jsonplaceholder.typicode.com/users')
   const users = await response.json();
